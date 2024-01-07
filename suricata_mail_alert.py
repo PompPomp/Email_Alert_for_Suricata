bd8bdecc38c083d086190b3c0894d518ef43d628
Wee Kang Lee <jasonleeweekang@gmail.com>
2024-01-07 11:24:16 +0800

    slight change in formatting

--- suricata_mail_alert.py
+++ suricata_mail_alert.py
@@ -3,34 +3,38 @@ import os
 import re
 import time
 
+
 def send_email(body, header, timedate):
     header = header[0].replace("(", "").replace(")", "")
-    
+
     body_details = list(body[0])
     message_log = body_details[1].replace("(", "").replace(")", "")
-    
-    command = f'echo "A new attack detected at {timedate}: {message_log}" | mail -s "Suricata detected a {header} attack" -a "From: suricata@terrasystems.com" jasonleeweekang@gmail.com'
+
+    command = f'echo "A new attack detected at {timedate}: {message_log}" | mail -s "Suricata detected a {header} ' \
+              f'attack" -a "From: suricata@terrasystems.com" jasonleeweekang@gmail.com '
     # Executing the mail command in the Linux command line
     os.system(command)
     print("Email alert sent")
 
-attacks=["1000001"]
+
+attacks = ["1000001"]
+
 
 def check_log_file(filename):
     with open(filename, "r") as f:
-        
+
         now = pd.to_datetime(pd.Timestamp.now())
         time.sleep(5)
         for line in f:
             event = pd.to_datetime(line[0:20])
-            try: 
+            try:
                 meta_pattern = r'\[\*\*\] \[(.*?)\] (.+?) \[\*\*\]'
                 metadata = re.findall(meta_pattern, line)
-                #print(metadata)
+                # print(metadata)
 
                 classification_pattern = r'\[Classification: (.*?)\]'
                 classification = re.findall(classification_pattern, line)
-                #print(classification)
+                # print(classification)
 
                 if re.findall(':1\d+:', line)[0].replace(':', '') in attacks and event < now:
                     send_email(metadata, classification, event)
@@ -41,9 +45,6 @@ def check_log_file(filename):
             except IndexError as ie:
                 print(ie)
 
+
 while True:
     check_log_file("./fast2.log")
-
-
-
-
